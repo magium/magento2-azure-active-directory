539d9a61deb030691ba7a87cce8e2b06c5f80134
Kevin Schroeder <kschroeder@mirageworks.com>
2017-04-20 09:55:44 -0500

    Updated to set the return URL in the config and force it explicitly instead of generating it.

--- src/Model/Auth.php
+++ src/Model/Auth.php
@@ -2,22 +2,30 @@
 
 namespace Magium\Magento2\AzureAD\Model;
 
+use Magento\Framework\App\Config\ScopeConfigInterface;
+
 class Auth
 {
     const CONFIG_ENABLED = 'magium/ad/enabled';
 
     protected $activeDirectory;
+    protected $config;
 
     public function __construct(
-        ActiveDirectory $activeDirectory
+        ActiveDirectory $activeDirectory,
+        ScopeConfigInterface $config
     )
     {
         $this->activeDirectory = $activeDirectory;
+        $this->config = $config;
     }
 
     public function beforeIsLoggedIn()
     {
         if ($this->activeDirectory->isEnabled()) {
+            $this->activeDirectory->setReturnUrl(
+                $this->config->getValue()
+            );
             $this->activeDirectory->authenticate();
         }
     }
